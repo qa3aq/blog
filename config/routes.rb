2add44693e49a0993bcb4d5d8cda6a933124bd2b
qa33aq <dimdac@gmail.com>
2020-12-30 15:51:07 +0500

    initialize repo

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  get 'home/index'
+
+  resources :articles
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
